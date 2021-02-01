918fb1755ef308d4c0c23a9346a801eee5d2583a
Booysen31SA <booysen31.matthew@gmail.com>
2021-02-01 17:29:19 +0200

    added dependencies and linked chatengine 10:42

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const projectID = "68eab922-9775-4d26-867e-49ef556cac46";
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = { projectID } 
+            userName = ""
+            userSecrect = ""
+        />
+    );
 }
 
 export default App;
